e393c17476b08a796d303c8a3b3a5b921cda8e6c
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-09-26 21:02:17 +0100

    Created component to show brand data

---
+++ src/brands.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// function brandList({ brand }) {
+function brandList() {
+  return (
+    <div>
+      <h1>Hello, 1</h1>
+    </div>
+  );
+}
+
+export default brandList;
