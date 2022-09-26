f4433128c47c4117de8eb437cfbc369895af1c11
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-09-26 21:06:53 +0100

    Added brands component to App file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import "./App.css";
+import React, { useState } from "react";
+
+import brandList from "./brands.js";
 
 function App() {
+  // const [brand, setBrand] = useState(["shut", "up"]);
+  // return <brandList brand={brand} />;
   return (
-    <div className="App">
-      <header className="App-header">
-        <p></p>
-      </header>
-    </div>
+    <>
+      <h1>Shit</h1>
+      <div>=</div>
+      <brandList />
+    </>
   );
 }
 
