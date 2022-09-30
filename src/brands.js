e472db1302ca2f45931d98b6b87ed5b81f363fc2
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-09-30 21:42:56 +0100

    Brands file now has code to hit api and retrieve data

--- src/brands.js
+++ src/brands.js
@@ -2,11 +2,29 @@ import React from "react";
 
 // function brandList({ brand }) {
 function brandList() {
+  const [brands, setBrands] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.example.com/brands")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setBrands(result);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }, []);
+
   return (
-    <div>
-      <h1>This the list of brands in the db</h1>
-    </div>
+    <ul>
+      {brands.map((brand) => (
+        <li key={brand.id}>
+          {brand.name} {brand.price}
+        </li>
+      ))}
+    </ul>
   );
 }
-
 export default brandList;
