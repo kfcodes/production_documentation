0841956ae61da830c01389c4717fcfa4871a8ad2
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-10-06 21:39:46 +0100

    Brands component hits api and displays data

--- src/brands.js
+++ src/brands.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 
 function BrandList() {
-  const [brands, setBrands] = useState(["test1", "test2"]);
+  const [list, setList] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:4040/brands/list")
       .then((res) => res.json())
       .then(
-        (result) => {},
+        (result) => {
+          setList(result.data);
+        },
         (error) => {
           console.log(error);
         }
@@ -15,11 +17,13 @@ function BrandList() {
   }, []);
 
   return (
-    <ul>
-      {brands.map((brand) => (
-        <p>{brand}</p>
-      ))}
-    </ul>
+    <div className="wrapper">
+      <ul>
+        {list.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 export default BrandList;
