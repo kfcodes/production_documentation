a32b49a7e7d8fe2677d3db26c20cacc27bd999a2
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-10-13 20:15:48 +0100

    Added react router and test routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
-
-import BrandList from "./brands.js";
+import { Routes, Route } from "react-router-dom";
+import BrandList from "./pages/brands.js";
+import BrandList2 from "./pages/brands2.js";
+import BrandList3 from "./pages/brands3.js";
 
 function App() {
-  // const [brand, setBrand] = useState(["shut", "up"]);
-  // return <brandList brand={brand} />;
   return (
     <>
-      <h1>This is A list of the brands in the Database</h1>
-      <BrandList />
+      <Routes>
+        <Route path="/" element={<BrandList />} />
+        <Route path="brands2" element={<BrandList2 />} />
+        <Route path="brands3" element={<BrandList3 />} />
+      </Routes>
     </>
   );
 }
