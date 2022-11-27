55db0e86be1d1341eea359ce80cf8c548a453c11
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-11-27 15:24:19 +0000

    Added products page to App file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import BrandList from "./pages/brands.js";
-import BrandList2 from "./pages/brands2.js";
-import BrandList3 from "./pages/brands3.js";
+import ProductList from "./pages/products.js";
 
+// Added the products page to react router
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<BrandList />} />
-        <Route path="brands2" element={<BrandList2 />} />
-        <Route path="brands3" element={<BrandList3 />} />
+        <Route path="/products" element={<ProductList />} />
       </Routes>
     </>
   );
