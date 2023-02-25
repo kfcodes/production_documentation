2f82c24946665776db27f9a25f5c062229a5c616
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-02-25 20:25:24 +0000

    Removed Unused imports from App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import BrandList from "./pages/brands.js";
-import Productsfrombrand from "./pages/products_from_brand.js";
 
 // Added the products by brand page to react router
 function App() {
@@ -9,10 +7,6 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<BrandList />} />
-        <Route
-          path="/products_from_brand/:brandid"
-          element={<Productsfrombrand />}
-        />
       </Routes>
     </>
   );
