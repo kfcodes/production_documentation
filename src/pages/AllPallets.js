444ec586c45077e1b033a07f0b2fa0d2548f1730
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-02-26 19:19:35 +0000

    Importing React UseState for pallet data

--- src/pages/AllPallets.js
+++ src/pages/AllPallets.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PalletList() {
+  const [pallets, setPallets] = useState([]);
 
   return (
     <div className="wrapper">
