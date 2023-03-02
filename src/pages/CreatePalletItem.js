1ae37b264573e58681afce272adff211e13fe7bf
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-03-02 21:45:02 +0000

    Created Create Pallet Item Component

---
+++ src/pages/CreatePalletItem.js
@@ -0,0 +1,10 @@
+// import { useForm } from "react-hook-form";
+import React, { useEffect, useState } from "react";
+
+export default function CreatePallet() {
+  return (
+    <>
+            <h1>Products on the pallet</h1>
+    </>
+  );
+}
