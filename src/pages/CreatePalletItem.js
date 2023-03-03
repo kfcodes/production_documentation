8ac5716bfac3f0020cdc11f6135d9f486d3f0823
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-03-03 19:55:13 +0000

    Adding imports and create context to get pallet id for create pallet item

--- src/pages/CreatePalletItem.js
+++ src/pages/CreatePalletItem.js
@@ -1,10 +1,36 @@
 // import { useForm } from "react-hook-form";
+import { useOutletContext } from "react-router-dom";
 import React, { useEffect, useState } from "react";
+import IndividualPalletItem from "./each_pallet_item";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import Input from "@mui/material/Input";
+import FilledInput from "@mui/material/FilledInput";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import Grid from "@mui/material/Unstable_Grid2";
+import Card from "@mui/material/Card";
+import Container from "@mui/material/Container";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 
 export default function CreatePallet() {
+  const palletId = useOutletContext();
+
+
   return (
     <>
             <h1>Products on the pallet</h1>
+    <p>{palletId}</p>
     </>
   );
 }
