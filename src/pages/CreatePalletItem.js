6cac2eb34857e9bfc3cd3fb92cb4aec9e588e56f
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-03-05 12:48:13 +0000

    Using Container and Cards to display the pallet item data

--- src/pages/CreatePalletItem.js
+++ src/pages/CreatePalletItem.js
@@ -72,7 +72,114 @@ export default function CreatePallet() {
     </div>
         {palletItems.map((product) => (
           <>
-          <p> {product}</p>
+            <Container>
+              <Card variant="outlined">
+                <CardContent>
+                  <form onSubmit={onSubmit}>
+                    <input
+                      id="item_id"
+                      name="item_id"
+                      type="hidden"
+                      value={product.item_id}
+                    />
+                    <Grid
+                      container
+                      padding={1}
+                      spacing={1}
+                      justifyContent="center"
+                    >
+                      <Grid item xs={12}>
+                        <TextField
+                          fullWidth
+                          type="text"
+                          disabled="true"
+                          id="product_description"
+                          name="product_description"
+                          value={product.product_description}
+                          inputProps={{ style: { textAlign: "center" } }}
+                        />
+                      </Grid>
+                    </Grid>
+                    <Grid
+                      container
+                      padding={1}
+                      spacing={1}
+                      justifyContent="center"
+                    >
+                      <Grid item xs={7}>
+                        <TextField
+                          fullWidth
+                          label="Product ID"
+                          type="text"
+                          id="product_id"
+                          name="product_id"
+                          value={product.pallet_item_product_id}
+                          inputProps={{ style: { textAlign: "center" } }}
+                        />
+                      </Grid>
+                      <Grid item xs={5}>
+                        <TextField
+                          fullWidth
+                          label="Quantity"
+                          type="number"
+                          id="quantity"
+                          name="quantity"
+                          value={product.quantity}
+                          inputProps={{ style: { textAlign: "center" } }}
+                        />
+                      </Grid>
+                    </Grid>
+                    <Grid
+                      container
+                      padding={1}
+                      spacing={1}
+                      justifyContent="center"
+                    >
+                      <Grid item xs={4}>
+                        <TextField
+                          fullWidth
+                          label="Lot"
+                          type="text"
+                          id="lot"
+                          name="lot"
+                          value={product.lot}
+                          inputProps={{ style: { textAlign: "center" } }}
+                        />
+                      </Grid>
+                      <Grid item xs={4}>
+                        <TextField
+                          fullWidth
+                          label="BBE"
+                          type="text"
+                          id="bbe"
+                          name="bbe"
+                          value={product.bbe}
+                          inputProps={{ style: { textAlign: "center" } }}
+                        />
+                      </Grid>
+                      <Grid item xs={4}>
+                        <TextField
+                          fullWidth
+                          label="Batch"
+                          type="text"
+                          id="batch"
+                          name="batch"
+                          value={product.batch}
+                          inputProps={{ style: { textAlign: "center" } }}
+                        />
+                      </Grid>
+                    </Grid>
+                    <Grid
+                      container
+                      padding={1}
+                      spacing={1}
+                      justifyContent="center"
+                    >
+                    </Grid>
+                  </form>
+                </CardContent>
+              </Card>
+            </Container>
           </>
         ))}
       </Box>
