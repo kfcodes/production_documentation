5248f342debfba2f88ea3511ef3958149d76ef59
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-03-09 20:38:19 +0000

    Imported the componets for the pallet data into App file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import PalletList from "./pages/AllPallets";
+import CreatePalletItem from "./pages/CreatePalletItem";
+import SinglePallet from "./pages/Pallet";
 
-// Added the products by brand page to react router
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<PalletList />} />
+        <Route path="pallet/:palletid" element={<SinglePallet />}>
+          <Route path="pallet_item" element={<CreatePalletItem />} />
+        </Route>
       </Routes>
     </>
   );
