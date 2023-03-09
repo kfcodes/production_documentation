5328d0d9c82e3eeba159bfee67d78971cab1c373
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-03-09 20:23:05 +0000

    Imported the imports that may be used in the component

--- src/pages/Pallet.js
+++ src/pages/Pallet.js
@@ -1,4 +1,21 @@
 import React from "react";
+import PrintLabeLButton from "./print_pallet_label_button";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import Input from "@mui/material/Input";
+import FilledInput from "@mui/material/FilledInput";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
 
 export default function SinglePallet() {
 
