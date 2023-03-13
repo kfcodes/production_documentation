ba813c718c0ad2d2142dc5fde6f64961bbde8ae8
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-03-13 20:28:38 +0000

    Update the Brand data that is shown to the end user

--- src/pages/brands.js
+++ src/pages/brands.js
@@ -19,17 +19,16 @@ function BrandList() {
 
   return (
     <div className="wrapper">
-      <h1>Brands</h1>
+      <h1>All Brands</h1>
       <ul>
         {brands.map((brand) => (
           <li key={brand.id}>
-            <Link to="/products_from_brand">
-              {brand.id} - {brand.name}
-            </Link>
+          <Link to={`/brand/${brand.prefix}`}><p> {brand.prefix} - {brand.name}</p></Link>
           </li>
         ))}
       </ul>
     </div>
   );
 }
+
 export default BrandList;
