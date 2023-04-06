a4d21dba990bdf54558589a68f72dc7b4095204e
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-06 15:11:29 +0100

    Created function to set the state with the pallet id

--- src/pages/CreatePallet.js
+++ src/pages/CreatePallet.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function CreateNewPallet() {
+  const [palletId, setPalletID] = useState("");
+
+  function createPallet() {
+    console.log("Created the pallet")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setPalletID(result);
+        }
+      );
+  }
 
   return (
     <>
