75765b0426b1955772fbac690d160dd920052ba6
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-08 20:57:11 +0100

    Created a component to print the pallet label

---
+++ src/pages/PrintPalletLabel.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function PrintLabeLButton() {
+
+  return (
+    <div className="wrapper">
+    <h1>
+    Print Label Button
+    </h1>
+    </div>
+  );
+
+}
