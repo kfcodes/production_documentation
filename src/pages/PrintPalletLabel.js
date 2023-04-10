21c51d86937bc6cf0bb761e33c4b1ad153284493
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-10 17:09:23 +0100

    Created a Print Pallet Function to take the id and print it to the console

--- src/pages/PrintPalletLabel.js
+++ src/pages/PrintPalletLabel.js
@@ -3,7 +3,12 @@ import React, { useEffect, useState } from "react";
 export default function PrintLabeLButton(id) {
 
 const pallet_id = id["id"];
-console.log(pallet_id);
+// console.log(pallet_id);
+
+const printPalletLabel = (pallet_id) => {
+  console.log("The Pallet id to be printed");
+  console.log(pallet_id);
+  };
 
   return (
     <div className="wrapper">
