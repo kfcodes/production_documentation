a47212ef6b8be9023c12ca7ccc3acb4feebdeefe
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-11 14:43:49 +0100

    Created a component to display the data for each pallet item

---
+++ src/pages/pallets/EachPalletItem.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function IndividualPalletItem() {
+  
+  return (
+    <div>
+    <h1>This component will display the details for each product</h1>
+    </div>
+  );
+}
