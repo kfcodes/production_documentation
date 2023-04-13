478f96de890b5d949a87d031691edbed98e9763f
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-13 21:55:50 +0100

    Created a component to show the total production

---
+++ src/pages/production/TotalProduction.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function FullMps() {
+
+  return (
+    <>
+    <h1>
+    This component will show the total production page
+    </h1>
+    </>
+  );
+}
+
+export default FullMps;
