5b294845d69dbd0dfaa95c88e7f7a2d625436d20
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-16 15:48:44 +0100

    Refactored the code for Upload pdf components

--- src/pages/labels/UploadPDFLables.js
+++ src/pages/labels/UploadPDFLables.js
@@ -1,13 +1,9 @@
-import React from "react";
-import "./UploadPDFLables.css";
-
-
-export default function UploadPdfLabel() {
+// drag drop file component
+function DragDropFile() {
+  // drag state
   const [dragActive, setDragActive] = React.useState(false);
-
-  //ref
-const inputRef = React.useRef(null);
-//...
+  // ref
+  const inputRef = React.useRef(null);
   
   // handle drag events
   const handleDrag = function(e) {
@@ -20,23 +16,39 @@ const inputRef = React.useRef(null);
     }
   };
   
-  // triggers the input when the button is clicked
-const onButtonClick = () => {
-  inputRef.current.click();
-};
+  // triggers when file is dropped
+  const handleDrop = function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+      // handleFiles(e.dataTransfer.files);
+    }
+  };
+  
+  // triggers when file is selected with click
+  const handleChange = function(e) {
+    e.preventDefault();
+    if (e.target.files && e.target.files[0]) {
+      // handleFiles(e.target.files);
+    }
+  };
+  
+// triggers the input when the button is clicked
+  const onButtonClick = () => {
+    inputRef.current.click();
+  };
   
   return (
     <form id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
-// add the ref to the input
-<input ref={inputRef} type="file" id="input-file-upload" multiple={true} onChange={handleChange} />
+      <input ref={inputRef} type="file" id="input-file-upload" multiple={true} onChange={handleChange} />
       <label id="label-file-upload" htmlFor="input-file-upload" className={dragActive ? "drag-active" : "" }>
         <div>
           <p>Drag and drop your file here or</p>
-          <button className="upload-button">Upload a file</button>
-<button className="upload-button" onClick={onButtonClick}>Upload a file</button>
+          <button className="upload-button" onClick={onButtonClick}>Upload a file</button>
         </div> 
-</label>
+      </label>
       { dragActive && <div id="drag-file-element" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}></div> }
-    </form>  );
-
+    </form>
+  );
 };
