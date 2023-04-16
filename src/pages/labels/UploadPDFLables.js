0b150e91dc2ec8351ed06a13198ef7c6e68c14c7
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-16 15:45:59 +0100

    Created button to open upload dialogue box

--- src/pages/labels/UploadPDFLables.js
+++ src/pages/labels/UploadPDFLables.js
@@ -4,6 +4,10 @@ import "./UploadPDFLables.css";
 
 export default function UploadPdfLabel() {
   const [dragActive, setDragActive] = React.useState(false);
+
+  //ref
+const inputRef = React.useRef(null);
+//...
   
   // handle drag events
   const handleDrag = function(e) {
@@ -16,13 +20,20 @@ export default function UploadPdfLabel() {
     }
   };
   
+  // triggers the input when the button is clicked
+const onButtonClick = () => {
+  inputRef.current.click();
+};
+  
   return (
     <form id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
-      <input type="file" id="input-file-upload" multiple={true} />
+// add the ref to the input
+<input ref={inputRef} type="file" id="input-file-upload" multiple={true} onChange={handleChange} />
       <label id="label-file-upload" htmlFor="input-file-upload" className={dragActive ? "drag-active" : "" }>
         <div>
           <p>Drag and drop your file here or</p>
           <button className="upload-button">Upload a file</button>
+<button className="upload-button" onClick={onButtonClick}>Upload a file</button>
         </div> 
 </label>
       { dragActive && <div id="drag-file-element" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}></div> }
