7277a93a1de8d353f26dbf2f2cb435ad362f964a
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-17 15:56:05 +0100

    Imported all the modules to use in the component

--- src/pages/pallets/RecentPallets.js
+++ src/pages/pallets/RecentPallets.js
@@ -1,4 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import CreateNewPallet from "./create_pallet";
+import List from "@mui/material/List";
+import Grid from "@mui/material/Unstable_Grid2";
+import ListItem from "@mui/material/ListItem";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import PropTypes from "prop-types";
+import Box from "@mui/material/Box";
+import Collapse from "@mui/material/Collapse";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Badge from "@mui/material/Badge";
+import Divider from "@mui/material/Divider";
+import Stack from "@mui/material/Stack";
+import Container from "@mui/material/Container";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 function PalletList() {
   return (
