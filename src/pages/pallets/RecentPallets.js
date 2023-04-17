1291763484769a00282b04d68c801c8a65170d25
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-17 15:34:13 +0100

    Created a component to display the most recently produced pallets

---
+++ src/pages/pallets/RecentPallets.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function PalletList() {
+  return (
+    <>
+    <h1>
+    This component shows the most recent pallets being produced
+    </h1>
+    </>
+  );
+}
+
+export default PalletList;
