094e763233551f1369f625ae0628d874ec5776c3
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-20 15:39:59 +0100

    Created Function to upload file to api and display the result

--- src/pages/labels/UploadPDFLables.js
+++ src/pages/labels/UploadPDFLables.js
@@ -7,23 +7,24 @@ export default function DragDropFile() {
   const [dragActive, setDragActive] = React.useState(false);
   const inputRef = React.useRef(null);
 
-  const UploadPdfFile = async (e) => {
-    console.log(file);
+  const UploadPdfFile = async () => {
     const formData = new FormData();
     formData.append("file", file);
-    // console.log("The file being uploaded is");
-    // console.log(file);
     fetch(`${process.env.REACT_APP_API_URL}/upload_pdf`, {
       method: "POST",
-      headers: {
-        'Accept': 'application/json',
-      },
-      body: FormData
+      // headers: {
+      //   'Content-Type': 'multipart/form-data'
+      // },
+      body: formData
+    //     console.log(file.name);
+    // fetch(`${process.env.REACT_APP_API_URL}/upload_pdf`, {
+    // method: "POST",
+    //   body: file
     })
     .then((res) => res.json())
-    .then(
-      (result) => {
-        console.log(result);
+    .then((result) => {
+        // console.log(uploaded);
+      setUploaded(result);
       }
     );
   };
@@ -44,9 +45,9 @@ export default function DragDropFile() {
     e.stopPropagation();
     setDragActive(false);
     if (e.dataTransfer.files && e.dataTransfer.files[0]) {
-      console.log("The File being Dropped is:");
-      console.log(e.dataTransfer.files);
-      setFile(e.dataTransfer.files);
+      // console.log("The File being Dropped is:");
+      // console.log(e.dataTransfer.files);
+      setFile(e.dataTransfer.files[0]);
     }
   };
 
@@ -81,55 +82,10 @@ export default function DragDropFile() {
   };
 
   return (
-    <>
-    {file != null ?
-      <p>
-      {file.name}
-      </p>
-    :
-      <form
-        id="form-file-upload"
-        onDragEnter={handleDrag}
-        onSubmit={(e) => e.preventDefault()}
-      >
-        <input
-          ref={inputRef}
-          type="file"
-          id="input-file-upload"
-          multiple={false}
-          onChange={handleChange}
-        />
-        <label
-          id="label-file-upload"
-          htmlFor="input-file-upload"
-          className={dragActive ? "drag-active" : ""}
-        >
-          <div>
-            <p>Drag and drop your file here or</p>
-            <button className="upload-button" onClick={onButtonClick}>
-              Upload a file
-            </button>
-          </div>
-        </label>
-        {dragActive && (
-          <div
-            id="drag-file-element"
-            onDragEnter={handleDrag}
-            onDragLeave={handleDrag}
-            onDragOver={handleDrag}
-            onDrop={handleDrop}
-          ></div>
-        )}
-      </form>
+    {file != null ? <> <h1> {file.name} </h1> <button className="uploadFile" onClick={UploadPdfFile}> Upload to server </button> <button className="showFile" onClick={ShowFile}> Show File </button> </>
+
+      : <><form id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()} > <input ref={inputRef} type="file" id="input-file-upload" multiple={false} onChange={handleChange} /> <label id="label-file-upload" htmlFor="input-file-upload" className={dragActive ? "drag-active" : ""} > <div> <p>Drag and drop your file here or</p> <button className="upload-button" onClick={onButtonClick}> Upload a file </button> </div> </label> {dragActive && ( <div id="drag-file-element" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop} ></div>)} </form></>
     }
-
-      <button className="uploadFile" onClick={UploadPdfFile}>
-        Upload to server
-      </button>
-
-      <button className="showFile" onClick={ShowFile}>
-        Show File
-      </button>
-    </>
+      </>
   );
 }
