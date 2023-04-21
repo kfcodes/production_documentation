6bb9c8767cf2484b7f8be30e82001ee869302c0b
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-21 15:03:02 +0100

    Fetching data from API and storing it in state

--- src/pages/documentation/ProductsFromBrand.js
+++ src/pages/documentation/ProductsFromBrand.js
@@ -1,6 +1,26 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 function ProductList() {
+  const [products, setProducts] = useState([]);
+  let { brandid } = useParams();
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/products_from_brand/${brandid}`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setProducts(result);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }, []);
+
+  console.log(brandid);
+  console.log(products);
 
   return (
     <div>
