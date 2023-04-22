e6e22c60c4f5743890289141b170a8d45e780ebf
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-22 16:35:08 +0100

    Created a component to display the data for each pallet

---
+++ src/pages/documentation/SinglePallet.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+
+function SinglePallet() {
+
+  return (
+    <div className="wrapper">
+      <h2>Single Pallet Components</h2>
+    </div>
+  );
+}
+
+export default SinglePallet;
