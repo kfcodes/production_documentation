41cb6b3b3c98e17fcaa1990d2ea04ac6f9d14d4d
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-22 15:33:37 +0100

    Making use of UseParams to the id of the product from the user

--- src/pages/documentation/BillOfMaterials.js
+++ src/pages/documentation/BillOfMaterials.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function Singlebom() {
+  let { productid } = useParams();
+
+console.log(productid);
 
   return (
-    <div>
+    <div className="wrapper">
       <h1>Product Components</h1>
     </div>
   );
