9a65c90547db4ba3ea9b7ed6a276bdadb0d36f4c
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-22 15:47:11 +0100

    Getting the Bill of Materials data from api and storing it in the component state

--- src/pages/documentation/BillOfMaterials.js
+++ src/pages/documentation/BillOfMaterials.js
@@ -2,9 +2,30 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Singlebom() {
+  const [bom, setBom] = useState([]);
   let { productid } = useParams();
+  // const url = process.env.REACT_APP_API_URL;
+  // const url = "http://localhost:4040"
+  // console.log(url);
+    // fetch(`http://localhost:4040/bom/${productid}`)
+    // fetch(`${url}/bom/${productid}`)
 
-console.log(productid);
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/bom/${productid}`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setBom(result);
+          console.log(result);
+          // console.log(`${process.env.REACT_APP_API_URL}/bom/${productid}`);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }, []);
+
+          console.log(bom);
 
   return (
     <div className="wrapper">
