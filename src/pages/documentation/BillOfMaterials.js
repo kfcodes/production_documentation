17652d5e0d633c821148ad9fcea5860f1151cc03
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-22 15:22:57 +0100

    Created a component to display the bill of materials for a product

---
+++ src/pages/documentation/BillOfMaterials.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+
+function Singlebom() {
+
+  return (
+    <div>
+      <h1>Product Components</h1>
+    </div>
+  );
+}
+export default Singlebom;
