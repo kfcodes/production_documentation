744e1796af575b39e5e1e7a6d8e0fa8ecf0debca
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-23 13:16:32 +0100

    Created a component to display the data of a single product

---
+++ src/pages/documentation/SingleProduct.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SingleProduct() {
+
+
+  return (
+    <div className="wrapper">
+      <h1>Single Product Component</h1>
+    </div>
+  );
+}
+export default SingleProduct;
