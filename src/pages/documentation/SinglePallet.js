d7caab2e694bf7d6714dcd03895945c95d7aa697
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-23 13:07:10 +0100

    Get the pallet data in store it in the state

--- src/pages/documentation/SinglePallet.js
+++ src/pages/documentation/SinglePallet.js
@@ -2,12 +2,52 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function SinglePallet() {
+  const [pallet, setPallet] = useState([]);
+  const [pallet_1, setPallet_1] = useState([]);
   let { palletid } = useParams();
-  console.log(palletid);
 
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/pallet/${palletid}`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setPallet(result);
+          setPallet_1(result[0]);
+          console.log(result);
+          // console.log(result.map());
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }, []);
+
+        // <p>{pallet.item_id}</p>
+        // <p>{pallet.pallet_item_pallet_id}</p>
+        // <p>{pallet.product_id}</p>
+        // <p>{pallet.lot}</p>
+        // <p>{pallet.bbe}</p>
+        // <p>{pallet.batch}</p>
+        // <p>{pallet.quantity}</p>
+        // <p>{pallet.id}</p>
+        // <p>{pallet.pallet_length}</p>
+        // <p>{pallet.pallet_width}</p>
+        // <p>{pallet.pallet_name}</p>
+        // <p>{pallet.packing_list_id}</p>
+        // <p>{pallet.packing_list_name}</p>
+        // {pallet.map((pallet) => (
+        // {pallet[0]((pallet) => (
+        // )
+// )}
+  //
+  //
+  //
+  //
+  // 
   return (
     <div className="wrapper">
       <h2> Pallet Details </h2>
+          <p>Pallet Number: {pallet_1.pallet_id}</p>
     </div>
   );
 }
