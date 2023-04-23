fa72572f970a02fe58ed251e952fe007477999b0
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-23 14:25:12 +0100

    Getting the Product data from the API and displaying it to the user

--- src/pages/documentation/SingleProduct.js
+++ src/pages/documentation/SingleProduct.js
@@ -1,13 +1,35 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 function SingleProduct() {
+  const [product, setProduct] = useState([]);
   let { productid } = useParams();
 
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/products/${productid}`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setProduct(result);
+          console.log(result);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }, []);
 
   return (
     <div className="wrapper">
-      <h1>{productid}</h1>
+      <h1>{product.product_id}</h1>
+      <h2>{product.product_description}</h2>
+      <p>Stock - {product.stock}</p>
+      <p>Lot - {product.lot_example}</p>
+      <p>BBE - {product.bbe_months} months</p>
+      <p>Batch - {product.batch}</p>
+      <p>Pallet Type - {product.pallet_name}</p>
+      <Outlet context={[product.product_id]}/>
     </div>
   );
 }
