7812e2963936f6b0381d19d9be5a922b19edc5a0
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-25 12:49:03 +0100

    Created a component to print the finished product labels

---
+++ src/pages/labels/BoxLabel.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+
+export default function Eol(props) {
+  return (
+    <>
+    <h1>
+    The Product Label Modal
+    </h1>
+
+    </>
+  );
+}
