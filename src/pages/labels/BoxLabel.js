88ee84fccae7812bfafbf9f97bb56efddec02e76
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-25 12:51:18 +0100

    Created a modal component to enter data

--- src/pages/labels/BoxLabel.js
+++ src/pages/labels/BoxLabel.js
@@ -1,12 +1,59 @@
+// import { useForm } from "react-hook-form";
+import { Outlet, useNavigate, redirect } from "react-router-dom";
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import PrintLabeLButton from "./print_pallet_label_button";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import IconButton from "@mui/material/IconButton";
+import Input from "@mui/material/Input";
+import FilledInput from "@mui/material/FilledInput";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import Button from "@mui/material/Button";
+import { __esModule } from "quagga";
+import Grid from "@mui/material/Grid";
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
 
 export default function Eol(props) {
+
   return (
     <>
-    <h1>
-    The Product Label Modal
-    </h1>
-
+      <Button
+    onClick={handleOpen}>
+    End Of line Data
+    </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Container maxWidth="sm">
+        <Box >
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+          Enter End of Line Data
+          </Typography>
+            <Grid container spacing={2}>
+              <Grid item xs={6} md={8}>
+                <TextField
+                  disabled
+                  label="PRODUCT CODE"
+                  type="text"
+                  value={productId}
+                />
+              </Grid>
+              </Grid>
+          </Box>
+        </Container>
+      </Modal>
     </>
   );
 }
