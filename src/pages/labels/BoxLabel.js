9000dcb402467bdfdfc700e4be370c646627a218
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-25 12:53:13 +0100

    Created inputs for the user to enter the data

--- src/pages/labels/BoxLabel.js
+++ src/pages/labels/BoxLabel.js
@@ -1,4 +1,3 @@
-// import { useForm } from "react-hook-form";
 import { Outlet, useNavigate, redirect } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -23,6 +22,72 @@ import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 
 export default function Eol(props) {
+  // const { register, handleSubmit, setValue } = useForm();
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [productCode, setProductCode] = useState("ProductCode");
+  const [customerPO, setCustomerPO] = useState("This is the Customer PO");
+  const [lot, setLot] = useState("");
+  const [bbe, setBbe] = useState("");
+  const [quantity, setQuantity] = useState(0);
+  const [productId] = useState(props.productId);
+  const [eolId] = useState(props.eolId);
+  // const [productId] = useState("pbn5000");
+  const [po] = useState(props.po);
+  // const [po] = useState("1");
+  const palletId = useParams();
+  const pId = palletId["palletid"];
+
+  console.log("Props");
+  // console.log(props.po);
+  // console.log(props.productId);
+  // console.log("product ID");
+  // console.log(productId);
+  // Get the details for the pallet
+  // useEffect(() => {
+  //   fetch(`${process.env.REACT_APP_API_URL}/pallet/${pId}`)
+  //     .then((res) => res.json())
+  //     .then(
+  //       (result) => {
+  //         setPallet_id(result[0].pallet_id);
+  //         setPalletType(result[0].pallet_type);
+  //         setEmptyweight(result[0].empty_weight);
+  //         setWeight(result[0].weight);
+  //         setHeight(result[0].height);
+  //         setPacking_list(result[0].packing_list);
+  //       },
+  //       (error) => {
+  //         console.log(error);
+  //       }
+  //     );
+  // }, []);
+
+  // const onSubmit = () => {
+  //   let palletData = {
+  //     pallet_type: palletType,
+  //     empty_weight: emptyweight,
+  //     weight: weight,
+  //     height: height,
+  //     packing_list: packing_list,
+  //   };
+  //   console.log(palletData);
+  //   fetch(`${process.env.REACT_APP_API_URL}/pallet/${pallet_id}`, {
+  //     method: "put",
+  //     mode: "cors",
+  //     headers: {
+  //       Accept: "application/json",
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(palletData),
+  //   }).then((res) => res.json());
+  // };
+  // <Box sx={{ flexGrow: 1 }}>
+  //
+    //   <Button
+    // onClick={handleOpen}>
+    // End Of line Data
+    // </Button>
 
   return (
     <>
@@ -50,7 +115,58 @@ export default function Eol(props) {
                   value={productId}
                 />
               </Grid>
+
+              <Grid item xs={6} md={8}>
+                <TextField
+                  disabled
+                  label="CUSTOMER PO"
+                  type="text"
+                  value={po}
+                />
               </Grid>
+              <Grid item xs={6} md={8}>
+                <TextField
+                  label="LOT"
+                  type="text"
+                  value={lot}
+                  onChange={(e) => setLot(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={6} md={8}>
+                <TextField
+                  label="BBE"
+                  type="text"
+                  value={bbe}
+                  onChange={(e) => setBbe(e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={6} md={8}>
+                <TextField
+                  label="Total Quantity Produced"
+                  type="number"
+                  value={quantity}
+                  onChange={(e) => setQuantity(e.target.value)}
+                  sx={{ m: 1, width: "25ch" }}
+                />
+              </Grid>
+              <Grid item xs={6} md={8}>
+                <Container maxWidth="sm">
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    size="Large"
+                    onClick={() => {
+                      console.log("pressed the button");
+                    }}
+                  >
+                    Save End of Line Sheet Details
+                  </Button>
+                  <Button >
+    Print Box Label
+                  </Button>
+                </Container>
+              </Grid>
+            </Grid>
           </Box>
         </Container>
       </Modal>
