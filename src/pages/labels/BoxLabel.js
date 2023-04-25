42841a9ff5045abd07253eec02371a7a867c309e
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-25 14:34:04 +0100

    Removed unused code from the print boxLabel component

--- src/pages/labels/BoxLabel.js
+++ src/pages/labels/BoxLabel.js
@@ -1,3 +1,4 @@
+// import { useForm } from "react-hook-form";
 import { Outlet, useNavigate, redirect } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -18,81 +19,68 @@ import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import Button from "@mui/material/Button";
 import { __esModule } from "quagga";
 import Grid from "@mui/material/Grid";
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
 
-const printPalletLabel = (id) => {
-  console.log(id);
-  fetch(`${process.env.REACT_APP_API_URL}/label/${id}`, {
-    method: "get",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  }).then((response) => {
-    console.log(response);
-  });
-};
+// const printPalletLabel = (id) => {
+//   console.log(id);
+//   fetch(`${process.env.REACT_APP_API_URL}/label/${id}`, {
+//     method: "get",
+//     headers: {
+//       Accept: "application/json",
+//       "Content-Type": "application/json",
+//     },
+//   }).then((response) => {
+//     console.log(response);
+//   });
+// };
+//
+// const printBoxLabel = (id) => {
+//   fetch(`${process.env.REACT_APP_API_URL}/box_label/${id}`, {
+//     method: "GET",
+//     headers: {
+//       Accept: "application/json",
+//       "Content-Type": "application/json",
+//     },
+//   }).then((response) => {
+//     console.log(response);
+//   });
+// };
 
-const printBoxLabel = (id) => {
-    fetch(`${process.env.REACT_APP_API_URL}/box_label/${id}`, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    }).then((response) => {
-      console.log(response);
-    });
-  };
+export default function PrintBoxLabel(props) {
 
-export default function Eol(props) {
-  // const { register, handleSubmit, setValue } = useForm();
+  const [eolId] = useState(props.eolId);
+  const [batch, setBatch] = useState("");
+  const [quantity, setQuantity] = useState(0);
+
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-  const [productCode, setProductCode] = useState("PBN5000");
-  const [customerPO, setCustomerPO] = useState("This is the Customer PO");
-  const [lot, setLot] = useState("");
-  const [bbe, setBbe] = useState("");
-  const [quantity, setQuantity] = useState(0);
-  const [productId] = useState(props.productId);
-  const [eolId] = useState(props.eolId);
-  // const [productId] = useState("pbn5000");
-  const [po] = useState(props.po);
-  // const [po] = useState("1");
-  const palletId = useParams();
-  const pId = palletId["palletid"];
 
-  console.log("Props");
-  // console.log(props.po);
-  // console.log(props.productId);
-  // console.log("product ID");
-  // console.log(productId);
-  // Get the details for the pallet
+  // Get the EOL details and add them to the state
   // useEffect(() => {
-  //   fetch(`${process.env.REACT_APP_API_URL}/pallet/${pId}`)
+  //   fetch(`${process.env.REACT_APP_API_URL}/eol/${eolId}`)
   //     .then((res) => res.json())
   //     .then(
   //       (result) => {
-  //         setPallet_id(result[0].pallet_id);
-  //         setPalletType(result[0].pallet_type);
-  //         setEmptyweight(result[0].empty_weight);
-  //         setWeight(result[0].weight);
-  //         setHeight(result[0].height);
-  //         setPacking_list(result[0].packing_list);
+  //         console.log(result);
+  //         setProductId(result[0].product_id);
+  //         setPo(result[0].po);
+  //         setLot(result[0].lot);
+  //         setBbe(result[0].bbe);
+  //         setQuantity(result[0].quantity);
   //       },
   //       (error) => {
   //         console.log(error);
@@ -100,117 +88,72 @@ export default function Eol(props) {
   //     );
   // }, []);
 
-  // const onSubmit = () => {
-  //   let palletData = {
-  //     pallet_type: palletType,
-  //     empty_weight: emptyweight,
-  //     weight: weight,
-  //     height: height,
-  //     packing_list: packing_list,
-  //   };
-  //   console.log(palletData);
-  //   fetch(`${process.env.REACT_APP_API_URL}/pallet/${pallet_id}`, {
-  //     method: "put",
-  //     mode: "cors",
-  //     headers: {
-  //       Accept: "application/json",
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify(palletData),
-  //   }).then((res) => res.json());
-  // };
-  // <Box sx={{ flexGrow: 1 }}>
-  //
-    //   <Button
-    // onClick={handleOpen}>
-    // End Of line Data
-    // </Button>
+  const onSubmit = () => {
+    let label = {
+  batch: batch,
+  label_quantity: quantity,
+    };
+    // console.log(label);
+  fetch(`${process.env.REACT_APP_API_URL}/box_label/${eolId}`, {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(label),
+    });
+    handleClose();
+  };
 
-  return (
-    <>
-      <Button
-    onClick={handleOpen}>
-    End Of line Data
-    </Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Container maxWidth="sm">
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-          Enter End of Line Data
-          </Typography>
-            <Grid container spacing={2}>
-              <Grid item xs={6} md={8}>
-                <TextField
-                  disabled
-                  label="PRODUCT CODE"
-                  type="text"
-                  value={productId}
-                />
+    return (
+      <>
+        <Button onClick={handleOpen}>Print Box Labels</Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Container maxWidth="sm">
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Enter Label data
+              </Typography>
+              <Grid container spacing={2}>
+                <Grid item xs={6} md={8}>
+                  <TextField
+                    label="Batch"
+                    type="text"
+                    value={batch}
+                    onChange={(e) => setBatch(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={6} md={8}>
+                  <TextField
+                    label="Quantity of Labels"
+                    type="number"
+                    value={quantity}
+                    onChange={(e) => setQuantity(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={6} md={8}>
+                  <Container maxWidth="sm">
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      size="Large"
+                      onClick={() => {
+                        onSubmit(eolId);
+                      }}
+                    >
+             PRINT LABELS
+                    </Button>
+                  </Container>
+                </Grid>
               </Grid>
-
-              <Grid item xs={6} md={8}>
-                <TextField
-                  disabled
-                  label="CUSTOMER PO"
-                  type="text"
-                  value={po}
-                />
-              </Grid>
-              <Grid item xs={6} md={8}>
-                <TextField
-                  label="LOT"
-                  type="text"
-                  value={lot}
-                  onChange={(e) => setLot(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={6} md={8}>
-                <TextField
-                  label="BBE"
-                  type="text"
-                  value={bbe}
-                  onChange={(e) => setBbe(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={6} md={8}>
-                <TextField
-                  label="Total Quantity Produced"
-                  type="number"
-                  value={quantity}
-                  onChange={(e) => setQuantity(e.target.value)}
-                  sx={{ m: 1, width: "25ch" }}
-                />
-              </Grid>
-              <Grid item xs={6} md={8}>
-                <Container maxWidth="sm">
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    size="Large"
-                    onClick={() => {
-                      // onSubmit();
-                      console.log("pressed the button");
-                    }}
-                  >
-                    Save End of Line Sheet Details
-                  </Button>
-                  <Button 
-        onClick={() => printBoxLabel(eolId)}>
-    Print Box Label
-                  </Button>
-                </Container>
-              </Grid>
-            </Grid>
-          </Box>
-        </Container>
-      </Modal>
-    </>
-  );
-}
-// <PrintLabeLButton id={pId} />
-// <Outlet context={[pId]} />
+            </Box>
+          </Container>
+        </Modal>
+      </>
+    );
+  }
