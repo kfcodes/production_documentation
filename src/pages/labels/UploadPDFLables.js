649ee22708504b1d4f4b3a9b9e767dbb54a092d3
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-25 18:07:15 +0100

    Modified the Upload PDF file to allow multiple uploads

--- src/pages/labels/UploadPDFLables.js
+++ src/pages/labels/UploadPDFLables.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState, useRef } from "react";
 import "./UploadPDFLables.css";
 
 export default function DragDropFile() {
+  const [file, setFile] = useState();
   const [files, setFiles] = useState([]);
   const [dragActive, setDragActive] = React.useState(false);
   const inputRef = React.useRef(null);
@@ -11,35 +12,34 @@ export default function DragDropFile() {
 
   const UploadPdfFile = async () => {
     const formData = new FormData();
-    formData.append("files", files);
-    fetch(`${process.env.REACT_APP_API_URL}/upload_pdf`, {
-      method: "POST",
-      // headers: {
-      //   'Content-Type': 'multipart/form-data'
-      // },
-      body: formData,
-      //     console.log(file.name);
-      // fetch(`${process.env.REACT_APP_API_URL}/upload_pdf`, {
-      // method: "POST",
-      //   body: file
-    })
-      .then((res) => res.json())
-      .then((result) => {
-        // console.log(uploaded);
-        setUploaded(result);
-      });
+    files.forEach((file) => 
+    formData.append("files[]", file))
+console.log(formData.getAll("files[]"))
+    setUploaded(formData.getAll("files[]"));
+    // fetch(`${process.env.REACT_APP_API_URL}/upload_pdf`, {
+    //   method: "POST",
+    //   // headers: {
+    //   //   'Content-Type': 'multipart/form-data'
+    //   // },
+    //   body: formData,
+    // })
+    //   .then((res) => res.json())
+    //   .then((result) => {
+    //     setUploaded(result);
+    //   });
   };
-  // 'Content-Type': 'multipart/form-data'
 
   const printFile = async () => {
-    fetch(`${process.env.REACT_APP_API_URL}/print_pdf/${files}`, {
-      method: "POST",
-    })
-      .then((res) => res.json())
-      .then((result) => {
-        console.log(result.message);
-        setPrinted(result.message);
-      });
+    files.forEach((file) => 
+    console.log(`${file.name} was printed`));
+    // fetch(`${process.env.REACT_APP_API_URL}/print_pdf/${file.name}`, {
+    //   method: "POST",
+    // })
+    //   .then((res) => res.json())
+    //   .then((result) => {
+    //     console.log(result.message);
+    //     setPrinted(result.message);
+    //   });
   };
 
   const handleDrag = function (e) {
@@ -56,122 +56,135 @@ export default function DragDropFile() {
     e.preventDefault();
     e.stopPropagation();
     setDragActive(false);
-    if (e.dataTransfer.files && e.dataTransfer.files) {
-      setFiles(e.dataTransfer.files);
+    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+      setFiles([...files, ...e.dataTransfer.files]);
     }
   };
 
   const handleChange = function (e) {
     e.preventDefault();
     if (e.target.files && e.target.files[0]) {
-      setFiles(e.target.files);
+      setFiles([...files, ...e.dataTransfer.files]);
     }
   };
 
-  const handleSave = function (e) {
-    // const onButtonClick = function(e) {
-    e.preventDefault();
-    if (e.target.files && e.target.files[0]) {
-      console.log(e.target.files[0]);
-      setFiles(e.target.files);
-    }
+  const onButtonClick = function () {
+    setFiles([...files, inputRef.current.click()]);
+    // console.log(file);
   };
 
-  const onButtonClick = function (e) {
-    // inputRef.current.click();
-    // console.log(inputRef.current.click());
-    // console.log(inputRef.current.click());
-    // e.preventDefault();
-    // setFile(e.target.files[0]);
-    setFiles(inputRef.current.click());
+  const showFile = function (e) {
+    // console.log(file);
     console.log(files);
-  };
-
-  const ShowFile = function (e) {
-    console.log(files);
-    console.log(uploaded);
+    // console.log(uploaded);
   };
 
   return (
     <>
-      {printed != null ? (
-        <>
+      {" "}
+      <button className="printFile" onClick={showFile}>
+        {" "}
+        Show File
+      </button>{" "}
+      <>
+        {" "}
+        <form
+          id="form-file-upload"
+          onDragEnter={handleDrag}
+          onSubmit={(e) => e.preventDefault()}
+        >
           {" "}
-          <h1>{printed}</h1>{" "}
-        </>
-      ) : (
-        <>
-          {uploaded != null ? (
-            <>
+          <input
+            ref={inputRef}
+            type="file"
+            id="input-file-upload"
+            multiple={true}
+            onChange={handleChange}
+          />{" "}
+          <label
+            id="label-file-upload"
+            htmlFor="input-file-upload"
+            className={dragActive ? "drag-active" : ""}
+          >
+            {" "}
+            <div>
               {" "}
-              <h1>To Print {files} Click the button below</h1>{" "}
-              <button className="printFile" onClick={printFile}>
+              <p>Drag and drop your PDF Label A5 file here or</p>{" "}
+              <button className="upload-button" onClick={onButtonClick}>
                 {" "}
-                PRINT THE LABELS{" "}
+                Upload a file{" "}
               </button>{" "}
-            </>
-          ) : (
-            <>
-              {" "}
-              {files.length  ? (
-                <>
-                  {" "}
-                  <h1> To Upload {files} click the button below</h1>{" "}
-                  <button className="uploadFile" onClick={UploadPdfFile}>
-                    {" "}
-                    UPLOAD FILE{" "}
-                  </button>{" "}
-                </>
-              ) : (
-                <>
-                  {" "}
-                  <form
-                    id="form-file-upload"
-                    onDragEnter={handleDrag}
-                    onSubmit={(e) => e.preventDefault()}
-                  >
-                    {" "}
-                    <input
-                      ref={inputRef}
-                      type="file"
-                      id="input-file-upload"
-                      multiple={false}
-                      onChange={handleChange}
-                    />{" "}
-                    <label
-                      id="label-file-upload"
-                      htmlFor="input-file-upload"
-                      className={dragActive ? "drag-active" : ""}
-                    >
-                      {" "}
-                      <div>
-                        {" "}
-                        <p>Drag and drop your PDF Label A5 file here or</p>{" "}
-                        <button
-                          className="upload-button"
-                          onClick={onButtonClick}
-                        >
-                          {" "}
-                          Upload a file{" "}
-                        </button>{" "}
-                      </div>{" "}
-                    </label>{" "}
-                    {dragActive && (
-                      <div
-                        id="drag-file-element"
-                        onDragEnter={handleDrag}
-                        onDragLeave={handleDrag}
-                        onDragOver={handleDrag}
-                        onDrop={handleDrop}
-                      ></div>
-                    )}{" "}
-                  </form>{" "}
-                </>
-              )}
-            </>
+            </div>{" "}
+          </label>{" "}
+          {dragActive && (
+            <div
+              id="drag-file-element"
+              onDragEnter={handleDrag}
+              onDragLeave={handleDrag}
+              onDragOver={handleDrag}
+              onDrop={handleDrop}
+            ></div>
           )}
-        </>
-      )}
+        </form>
+      </>
+      <>
+        {printed != null ? (<> <h1>{printed}</h1> </>) : (<> <h1> Not Printed </h1> </>)}
     </>
+
+      <>
+            {uploaded != null ? (
+              <>
+                <h1>To Print File Click the button below</h1>{" "}
+                <button className="printFile" onClick={printFile}>
+                  PRINT THE LABELS{" "}
+                </button>{" "}
+              </>
+            ) : (
+                <>
+                  <h1> To Upload File click the button below</h1>
+                  <button className="uploadFile" onClick={UploadPdfFile}>
+                    UPLOAD FILE
+                  </button>
+                </>
+            )}
+          </>
+      </>
   );
 }
+
+
+// {file != null ? (
+//
+      // <>
+      //   {printed != null ? (
+      //     <>
+      //       {" "}
+      //       <h1>{printed}</h1>{" "}
+      //     </>
+      //   ) : (
+      //     <>
+      //       {uploaded != null ? (
+      //         <>
+      //           {" "}
+      //           <h1>To Print {file.name} Click the button below</h1>{" "}
+      //           <button className="printFile" onClick={printFile}>
+      //             {" "}
+      //             PRINT THE LABELS{" "}
+      //           </button>{" "}
+      //         </>
+      //       ) : (
+      //         <>
+      //           {" "}
+      //           <>
+      //             {" "}
+      //             <h1> To Upload {file.name} click the button below</h1>{" "}
+      //             <button className="uploadFile" onClick={UploadPdfFile}>
+      //               {" "}
+      //               UPLOAD FILE{" "}
+      //             </button>{" "}
+      //           </>
+      //         </>
+      //       )}
+      //     </>
+      //   )}
+      // </>
