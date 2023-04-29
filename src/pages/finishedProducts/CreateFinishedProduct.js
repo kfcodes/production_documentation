01ba464bcf671112e5506457cecc152730f362c1
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-04-29 23:20:29 +0100

    Created a component to create new finished product

---
+++ src/pages/finishedProducts/CreateFinishedProduct.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+
+
+export default function CreateEol() {
+
+  return (
+    <>
+    <h1>
+    Create Finished product data
+    </h1>
+    </>
+  );
+}
