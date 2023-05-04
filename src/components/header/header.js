777da698505307a67a17ffaea31763ac25c38c51
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-05-04 15:09:27 +0100

    Added code to display the links to the user in the header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,97 @@
 import React, { useEffect, useState } from "react";
+import CreateNewPallet from "./CreatePallet";
+import Grid from "@mui/material/Unstable_Grid2";
+import Button from "@mui/material/Button";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Collapse from "@mui/material/Collapse";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Divider from "@mui/material/Divider";
+import Stack from "@mui/material/Stack";
+import Container from "@mui/material/Container";
+
+
+
+
+
+function Header() {
 
   return (
     <>
-	  <h1>
-	  Header component
-	  </h1>
+      <Box component="span" sx={{ p: 2, border: "1px dashed grey" }}>
+        <Grid container padding={2} spacing={1} justifyContent="center">
+          <Grid item alignItems="center">
+            <AppBar position="absolute" component="nav" color="primary">
+              <Toolbar>
+                <Container>
+                  <Stack
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                    spacing={4}
+                    divider={<Divider orientation="vertical" flexItem />}
+                  >
+                    <Button
+                      href="/"
+                      fullWidth
+                      size="large"
+                      color="warning"
+                      variant="contained"
+                      xs={style}
+                    >
+                      PALLETS
+                    </Button>
+                    <Button
+                      href="/mps/"
+                      fullWidth
+                      size="large"
+                      color="warning"
+                      variant="contained"
+                    >
+                      PRODUCTION
+                    </Button>
+                  </Stack>
+                </Container>
+              </Toolbar>
+            </AppBar>
+          </Grid>
+        </Grid>
+        <Container>
+        <Grid container padding={2} spacing={3} justifyContent="center">
+          <Grid item alignItems="center">
+          <Stack
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            spacing={4}
+            divider={<Divider orientation="vertical" flexItem />}
+          >
+            <Button
+                    onClick={() => handleOnExport()}
+              size="large"
+              color="primary"
+              variant="contained"
+            >
+            EXPORT PACKING LIST
+            </Button>
+            <CreateNewPallet />
+          </Stack>
+          </Grid>
+        </Grid>
+        </Container>
+      </Box>
     </>
   );
 }
+
+export default Header;
