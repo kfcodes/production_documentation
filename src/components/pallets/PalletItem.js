871e47ba259090823654a021c75bb241266c619c
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-05-05 14:45:57 +0100

    Refactored Components for pallet items

--- src/components/pallets/PalletItem.js
+++ src/components/pallets/PalletItem.js
@@ -1,47 +1,17 @@
 import { useOutletContext } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Unstable_Grid2";
 import Card from "@mui/material/Card";
 import Container from "@mui/material/Container";
 import CardContent from "@mui/material/CardContent";
 import DeletePalletItem from "./DeletePalletItemButton";
-import CreateNewPalletItem from "./CreateNewPalletItemButton";
 
-export default function CreatePallet() {
-  const palletId = useOutletContext();
-  const [palletItems, setPalletItems] = useState([]);
-  const [newPalletItems, setNewPalletItems] = useState([]);
-
-  const createNewPalletItem = (pallet) => {
-    let palletItemData = {
-      pallet_item_pallet_id: pallet,
-    };
-    fetch(`${process.env.REACT_APP_API_URL}/pallet_item/${pallet}`, {
-      method: "post",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(palletItemData),
-    }).then((result) => {
-      setNewPalletItems(result);
-    });
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(
-        `${process.env.REACT_APP_API_URL}/pallet_items/${palletId}`
-      );
-      const result = await response.json();
-      setPalletItems(result);
-      console.log("The state should be updated");
-    };
-    fetchData();
-  }, [newPalletItems]);
+export default function PalletItem(props) {
+  const product = props["product"];
+  const palletId = props["palletId"];
+  const setNewPalletItems = props["setNewPalletItems"];
 
   const onSubmit = (event) => {
     event.preventDefault();
@@ -72,153 +42,112 @@ export default function CreatePallet() {
 
   return (
     <>
-      <Box sx={{ display: "flex", flexWrap: "wrap" }}>
-        <Container>
-          <div>
-            <Grid container padding={1} spacing={1} justifyContent="center">
-              <Grid item>
-                <h1>PALLET ITEMS</h1>
-              </Grid>
-            </Grid>
-          </div>
-        </Container>
-      </Box>
-      <Box sx={{ display: "flex", flexWrap: "wrap" }}>
-        {palletItems.map((product) => (
-          <>
-            <Container>
-              <Card variant="outlined">
-                <CardContent>
-                  <form onSubmit={onSubmit}>
-                    <input
-                      id="item_id"
-                      name="item_id"
-                      type="hidden"
-                      value={product.item_id}
+      <Container>
+        <Card variant="outlined">
+          <CardContent>
+            <form onSubmit={onSubmit}>
+              <input
+                id="item_id"
+                name="item_id"
+                type="hidden"
+                value={product.item_id}
+              />
+              <Grid container padding={1} spacing={1} justifyContent="center">
+                {product.product_description != null && (
+                  <Grid item xs={12}>
+                    <TextField
+                      fullWidth
+                      type="text"
+                      disabled="true"
+                      id="product_description"
+                      name="product_description"
+                      value={product.product_description}
+                      inputProps={{ style: { textAlign: "center" } }}
                     />
-                    <Grid
-                      container
-                      padding={1}
-                      spacing={1}
-                      justifyContent="center"
-                    >
-                      {product.product_description != null && (
-                        <Grid item xs={12}>
-                          <TextField
-                            fullWidth
-                            type="text"
-                            disabled="true"
-                            id="product_description"
-                            name="product_description"
-                            value={product.product_description}
-                            inputProps={{ style: { textAlign: "center" } }}
-                          />
-                        </Grid>
-                      )}
-                    </Grid>
-                    <Grid
-                      container
-                      padding={1}
-                      spacing={1}
-                      justifyContent="center"
-                    >
-                      <Grid item xs={7}>
-                        <TextField
-                          fullWidth
-                          label="Product ID"
-                          type="text"
-                          id="product_id"
-                          name="product_id"
-                          value={product.pallet_item_product_id}
-                          inputProps={{ style: { textAlign: "center" } }}
-                        />
-                      </Grid>
-                      <Grid item xs={5}>
-                        <TextField
-                          fullWidth
-                          label="Quantity"
-                          type="number"
-                          id="quantity"
-                          name="quantity"
-                          value={product.quantity}
-                          inputProps={{ style: { textAlign: "center" } }}
-                        />
-                      </Grid>
-                    </Grid>
-                    <Grid
-                      container
-                      padding={1}
-                      spacing={1}
-                      justifyContent="center"
-                    >
-                      <Grid item xs={4}>
-                        <TextField
-                          fullWidth
-                          label="Lot"
-                          type="text"
-                          id="lot"
-                          name="lot"
-                          value={product.lot}
-                          inputProps={{ style: { textAlign: "center" } }}
-                        />
-                      </Grid>
-                      <Grid item xs={4}>
-                        <TextField
-                          fullWidth
-                          label="BBE"
-                          type="text"
-                          id="bbe"
-                          name="bbe"
-                          value={product.bbe}
-                          inputProps={{ style: { textAlign: "center" } }}
-                        />
-                      </Grid>
-                      <Grid item xs={4}>
-                        <TextField
-                          fullWidth
-                          label="Batch"
-                          type="text"
-                          id="batch"
-                          name="batch"
-                          value={product.batch}
-                          inputProps={{ style: { textAlign: "center" } }}
-                        />
-                      </Grid>
-                    </Grid>
-                    <Grid
-                      container
-                      padding={1}
-                      spacing={1}
-                      justifyContent="center"
-                    >
-                      <DeletePalletItem
-                        id={product.item_id}
-                        state={setNewPalletItems}
-                      />
-                      <Grid xs={4}>
-                        {" "}
-                        <Button
-                          type="submit"
-                          variant="contained"
-                          color="secondary"
-                          size="large"
-                          sx={{ marginLeft: "auto" }}
-                        >
-                          SAVE
-                        </Button>
-                      </Grid>
-                    </Grid>
-                  </form>
-                </CardContent>
-              </Card>
-            </Container>
-          </>
-        ))}
-      </Box>
-                      <CreateNewPalletItem
-                        id={palletId}
-                        state={setNewPalletItems}
-                      />
+                  </Grid>
+                )}
+              </Grid>
+              <Grid container padding={1} spacing={1} justifyContent="center">
+                <Grid item xs={7}>
+                  <TextField
+                    fullWidth
+                    label="Product ID"
+                    type="text"
+                    id="product_id"
+                    name="product_id"
+                    value={product.pallet_item_product_id}
+                    inputProps={{ style: { textAlign: "center" } }}
+                  />
+                </Grid>
+                <Grid item xs={5}>
+                  <TextField
+                    fullWidth
+                    label="Quantity"
+                    type="number"
+                    id="quantity"
+                    name="quantity"
+                    value={product.quantity}
+                    inputProps={{ style: { textAlign: "center" } }}
+                  />
+                </Grid>
+              </Grid>
+              <Grid container padding={1} spacing={1} justifyContent="center">
+                <Grid item xs={4}>
+                  <TextField
+                    fullWidth
+                    label="Lot"
+                    type="text"
+                    id="lot"
+                    name="lot"
+                    value={product.lot}
+                    inputProps={{ style: { textAlign: "center" } }}
+                  />
+                </Grid>
+                <Grid item xs={4}>
+                  <TextField
+                    fullWidth
+                    label="BBE"
+                    type="text"
+                    id="bbe"
+                    name="bbe"
+                    value={product.bbe}
+                    inputProps={{ style: { textAlign: "center" } }}
+                  />
+                </Grid>
+                <Grid item xs={4}>
+                  <TextField
+                    fullWidth
+                    label="Batch"
+                    type="text"
+                    id="batch"
+                    name="batch"
+                    value={product.batch}
+                    inputProps={{ style: { textAlign: "center" } }}
+                  />
+                </Grid>
+              </Grid>
+              <Grid container padding={1} spacing={1} justifyContent="center">
+                <DeletePalletItem
+                  id={product.item_id}
+                  state={setNewPalletItems}
+                />
+                <Grid xs={4}>
+                  {" "}
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    color="secondary"
+                    size="large"
+                    sx={{ marginLeft: "auto" }}
+                  >
+                    SAVE
+                  </Button>
+                </Grid>
+              </Grid>
+            </form>
+          </CardContent>
+        </Card>
+      </Container>
     </>
   );
 }
---
+++ src/components/pallets/PalletItemsList.js
@@ -0,0 +1,85 @@
+import { useOutletContext } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Unstable_Grid2";
+import Card from "@mui/material/Card";
+import Container from "@mui/material/Container";
+import CardContent from "@mui/material/CardContent";
+import DeletePalletItem from "./DeletePalletItemButton";
+import CreateNewPalletItem from "./CreateNewPalletItemButton";
+import PalletItem from "./PalletItem";
+
+export default function CreatePallet() {
+  const palletId = useOutletContext();
+  const [palletItems, setPalletItems] = useState([]);
+  const [newPalletItems, setNewPalletItems] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/pallet_items/${palletId}`
+      );
+      const result = await response.json();
+      setPalletItems(result);
+      console.log("The state should be updated");
+    };
+    fetchData();
+  }, [newPalletItems]);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    let palletItemData = {
+      pallet_item_pallet_id: palletId[0],
+      item_id: event.target.item_id.value,
+      pallet_item_product_id: event.target.product_id.value,
+      quantity: event.target.quantity.value,
+      lot: event.target.lot.value,
+      bbe: event.target.bbe.value,
+      batch: event.target.batch.value,
+    };
+    console.log(palletItemData);
+    fetch(
+      `${process.env.REACT_APP_API_URL}/pallet_item/${palletItemData.item_id}`,
+      {
+        method: "put",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(palletItemData),
+      }
+    ).then((result) => {
+      setNewPalletItems(result);
+    });
+  };
+
+  return (
+    <>
+      <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+        <Container>
+          <div>
+            <Grid container padding={1} spacing={1} justifyContent="center">
+              <Grid item>
+                <h1>PALLET ITEMS</h1>
+              </Grid>
+            </Grid>
+          </div>
+        </Container>
+      </Box>
+      <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+        {palletItems.map((product) => (
+          <>
+            <PalletItem
+              product={product}
+              palletId={palletId}
+              setNewPalletItems={setNewPalletItems}
+            />
+          </>
+        ))}
+      </Box>
+      <CreateNewPalletItem id={palletId} state={setNewPalletItems} />
+    </>
+  );
+}
