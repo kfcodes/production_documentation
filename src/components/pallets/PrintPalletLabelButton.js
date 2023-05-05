c4e11a5f42c2c8b5d56d37914b50e6564ea13022
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-05-05 11:28:33 +0100

    Refactored and renamed files for clarity

---
+++ src/components/pallets/PrintPalletLabelButton.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+export default function PrintLabeLButton(id) {
+  const pallet_id = id["id"];
+
+  const printPalletLabel = (pallet_id) => {
+    fetch(`${process.env.REACT_APP_API_URL}/label/${pallet_id}`, {
+      method: "get",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+
+  return (
+    <div className="wrapper">
+      <Button
+        variant="contained"
+        size="small"
+        color="secondary"
+        onClick={() => {
+          printPalletLabel(pallet_id);
+        }}
+      >
+        PRINT PALLET LABEL
+      </Button>
+    </div>
+  );
+}
--- src/components/pallets/SinglePalletDetails.js
+++ src/components/pallets/SinglePalletDetails.js
@@ -3,18 +3,9 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import PrintLabeLButton from "./PrintPalletLabel";
 import Box from "@mui/material/Box";
-import IconButton from "@mui/material/IconButton";
-import Input from "@mui/material/Input";
-import FilledInput from "@mui/material/FilledInput";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import InputLabel from "@mui/material/InputLabel";
 import InputAdornment from "@mui/material/InputAdornment";
 import Container from "@mui/material/Container";
-import FormHelperText from "@mui/material/FormHelperText";
-import FormControl from "@mui/material/FormControl";
 import TextField from "@mui/material/TextField";
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import MenuItem from "@mui/material/MenuItem";
@@ -49,7 +40,6 @@ const style = {
 };
 
 export default function SinglePallet() {
-  const [pallet, setPallet] = useState({});
   const [pallet_id, setPallet_id] = useState(0);
   const [palletType, setPalletType] = useState(0);
   const [emptyweight, setEmptyweight] = useState(0);
@@ -59,7 +49,6 @@ export default function SinglePallet() {
   const palletId = useParams();
   const pId = palletId["palletid"];
 
-  // Get the details for the pallet
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_URL}/pallet/${pId}`)
       .then((res) => res.json())
