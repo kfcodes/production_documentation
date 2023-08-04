f64d620c426213fb6e8d2d0401eadd734a249fb8
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-04 15:08:52 +0100

    Removed unused code from the po component

--- src/components/po/CreatePo.js
+++ src/components/po/CreatePo.js
@@ -1,26 +1,10 @@
-// import { useForm } from "react-hook-form";
-import { Outlet, useNavigate, redirect } from "react-router-dom";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import PrintLabeLButton from "./print_pallet_label_button";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import IconButton from "@mui/material/IconButton";
-import Input from "@mui/material/Input";
-import FilledInput from "@mui/material/FilledInput";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import InputLabel from "@mui/material/InputLabel";
-import InputAdornment from "@mui/material/InputAdornment";
-import FormHelperText from "@mui/material/FormHelperText";
-import FormControl from "@mui/material/FormControl";
 import TextField from "@mui/material/TextField";
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import Button from "@mui/material/Button";
-import { __esModule } from "quagga";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import Modal from "@mui/material/Modal";
 
 // const style = {
 //   position: "absolute",
