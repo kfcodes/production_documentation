1a2c04f2bfa9862b82457fd86816d3a660cbb1dc
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-08 13:03:32 +0100

    The combined pallet module retrieves the possible pallets to combine from the DB

--- src/components/pallets/combinePallets/CombinePallets.js
+++ src/components/pallets/combinePallets/CombinePallets.js
@@ -5,14 +5,20 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Header from "../../header/Header";
 
-export default function Po() {
+export default function CombinePallets() {
   const [possiblePallets, setPossiblePallets] = useState([]);
-  const [pallets, setPallets] = useState(0);
-  const [height, setHeight] = useState("");
 
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_URL}/eol/${eolId}`)
+    fetch(`${process.env.REACT_APP_API_URL}/possible_pallets`)
       .then((res) => res.json())
       .then(
         (result) => {
@@ -24,63 +30,10 @@ export default function Po() {
       );
   }, []);
 
-  const onSubmit = () => {
-      let po = {
-    po: poCode,
-    customerCode: customerCode,
-      };
-    // console.log(label);
-    fetch(`${process.env.REACT_APP_API_URL}/po/`, {
-      method: "post",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(po),
-    });
-  };
 
   return (
     <>
-      <Container maxWidth="sm">
-        <Box>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-    ENTER IDS OF PALLETS TO COMBINE 
-          </Typography>
-          <Grid container spacing={2}>
-            <Grid item xs={6} md={8}>
-              <TextField
-                label="PALLET ID"
-                type="number"
-                value={pallet_id}
-                onChange={(e) => setPallets(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={6} md={8}>
-              <TextField
-                label="TOTAL PALLET HEIGHT"
-                type="number"
-                value={heigh}
-                onChange={(e) => setHeight(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={6} md={8}>
-              <Container maxWidth="sm">
-                <Button
-                  variant="contained"
-                  color="primary"
-                  size="Large"
-                  onClick={() => {
-                    onSubmit();
-                  }}
-                >
-                SAVE AND PRINT LABEL
-                </Button>
-              </Container>
-            </Grid>
-          </Grid>
-        </Box>
-      </Container>
+      <Header />
     </>
   );
 }
