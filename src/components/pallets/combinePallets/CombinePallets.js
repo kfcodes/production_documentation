6018c0a2f98881c0a1a5b89f30f59d98b54d9317
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-08 16:54:45 +0100

    The data for the pallets on the list are show to the user

--- src/components/pallets/combinePallets/CombinePallets.js
+++ src/components/pallets/combinePallets/CombinePallets.js
@@ -17,7 +17,7 @@ import Header from "../../header/Header";
 export default function CombinePallets() {
   const [possiblePallets, setPossiblePallets] = useState([]);
   const [palletId, setPalletId] = useState(0);
-  const [palletData, setPalletData] = useState([]);
+  const [palletDataArray, setPalletDataArray] = useState([]);
   const [selectedpallets, setSelectePallets] = useState([]);
   const [height, setHeight] = useState(0);
 
@@ -34,94 +34,90 @@ export default function CombinePallets() {
       );
   }, []);
 
-  const addPallet = (id) => {
-    if (possiblePallets.includes(id))
-      fetch(`${process.env.REACT_APP_API_URL}/pallet/${id}`).then((res) => res.json())
-        .then((palletData) => {
-          palletData.push(palletData);
-          console.log(palletData);
-        });
+  const addPallets2 = () => {
+    console.log(selectedpallets);
+    console.log(palletDataArray);
   };
+
+  const addPallet = (id) => {
+    if (possiblePallets.includes(id)) console.log(possiblePallets);
+    fetch(`${process.env.REACT_APP_API_URL}/pallet_details/${id}`)
+      .then((res) => res.json())
+      .then((palletData) => {
+        setSelectePallets([...selectedpallets, id]);
+        setPalletDataArray([...palletDataArray, ...palletData]);
+      });
+  };
+
+  // <Grid item xs={6} md={8}>
+  //   <TextField
+  //     label="TOTAL PALLET HEIGHT"
+  //     type="number"
+  //     value={height}
+  //     onChange={(e) => setHeight(e.target.value)}
+  //   />
+  // </Grid>
 
   return (
     <>
       <Header />
-
       <Container maxWidth="sm">
         <Box>
-    
+
           <Typography id="modal-modal-title" variant="h6" component="h2">
             PALLETS BEING COMBINED
           </Typography>
-
 
           <Grid container padding={2} spacing={1} justifyContent="center">
             <Grid item alignItems="center"></Grid>
           </Grid>
 
-          <TableContainer component={Paper}>
-            <Table aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell align="center">PALLET</TableCell>
-                  <TableCell align="center">SIZE (CM)</TableCell>
-                  <TableCell align="center">HEIGHT (CM)</TableCell>
-                  <TableCell align="center">WEIGHT (KG)</TableCell>
-                  <TableCell />
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {palletData.map((p) => (
-                  <>
-                    <TableRow key={p.pallet_id}>
-                      <TableCell align="center">{p.id}</TableCell>
-                      <TableCell align="left">{p.size}</TableCell>
-                      <TableCell align="right">{p.height}</TableCell>
-                      <TableCell align="left">{p.weight}</TableCell>
-                    </TableRow>
-                  </>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+          <Grid container padding={2} spacing={1} justifyContent="center">
+            <TableContainer component={Paper}>
+              <Table aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="center">PALLET</TableCell>
+                    <TableCell align="center">HEIGHT (CM)</TableCell>
+                    <TableCell align="center">WEIGHT (KG)</TableCell>
+                    <TableCell />
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {palletDataArray.map((p) => (
+                    <>
+                      <TableRow key={p.pallet_id}>
+                        <TableCell align="center">{p.pallet_id}</TableCell>
+                        <TableCell align="center">{p.height}</TableCell>
+                        <TableCell align="center">{p.weight}</TableCell>
+                      </TableRow>
+                    </>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
 
-          <Grid container spacing={2}>
-
+          <Grid container spacing={20}>
             <Grid item xs={6} md={8}>
-
               <TextField
                 label="PALLET ID"
                 type="number"
                 value={palletId}
                 onChange={(e) => setPalletId(e.target.value)}
               />
-
             </Grid>
-
             <Grid item xs={6} md={8}>
-
-              <TextField
-                label="TOTAL PALLET HEIGHT"
-                type="number"
-                value={height}
-                onChange={(e) => setHeight(e.target.value)}
-              />
-
-            </Grid>
-
-            <Grid item xs={6} md={8}>
-
               <Container maxWidth="sm">
-
                 <Button
                   variant="contained"
                   color="primary"
                   size="Large"
                   onClick={() => {
-                    addPallet(3104);
+                    addPallet(palletId);
                   }}
                 >
-    ADD PALLET TO LIST
+                  ADD PALLET
                 </Button>
 
                 <Button
@@ -129,18 +125,14 @@ export default function CombinePallets() {
                   color="error"
                   size="Large"
                   onClick={() => {
-                    addPallet(3104);
+                    addPallets2();
                   }}
                 >
-    COMBINE PALLETS AND PRINT LABEL
+                  COMBINE PALLETS AND PRINT LABEL
                 </Button>
-
               </Container>
-
             </Grid>
-
           </Grid>
-
         </Box>
       </Container>
     </>
