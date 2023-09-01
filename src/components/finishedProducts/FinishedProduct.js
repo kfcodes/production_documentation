4df5f6b97ae0f2658536501620d796d8f9744ab3
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-01 15:25:07 +0100

    Improved formatting for the module for entering the box label data

--- src/components/finishedProducts/FinishedProduct.js
+++ src/components/finishedProducts/FinishedProduct.js
@@ -33,28 +33,28 @@ export default function FinsishedProduct() {
   const handleClose = () => setOpen(false);
 
   const onSubmitId = () => {
-    fetch(`${process.env.REACT_APP_API_URL}/label_info/${productId}`)
-      .then((res) => {
-          if (res.status == 200) {
-        fetch(`${process.env.REACT_APP_API_URL}/product/${productId}`)
-          .then((res) => res.json())
-          .then(
-            (result) => {
-              setProductDescription(result.product_description);
-            },
-            (error) => {
-              console.log(error);
-            }
-          )
-          } else {
-            setNo("No Lable found");
-            return;
-          }
-        },
-        (error) => {
-          console.log(error);
+    fetch(`${process.env.REACT_APP_API_URL}/label_info/${productId}`).then(
+      (res) => {
+        if (res.status == 200) {
+          fetch(`${process.env.REACT_APP_API_URL}/product/${productId}`)
+            .then((res) => res.json())
+            .then(
+              (result) => {
+                setProductDescription(result.product_description);
+              },
+              (error) => {
+                console.log(error);
+              }
+            );
+        } else {
+          setNo("No Lable found");
+          return;
         }
-      )
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   };
 
   const onSubmitData = () => {
@@ -81,8 +81,8 @@ export default function FinsishedProduct() {
   };
 
   const onPrintLabels = () => {
-    console.log(useId)
-    let newnumber = { qty: qty, };
+    console.log(useId);
+    let newnumber = { qty: qty };
     fetch(`${process.env.REACT_APP_API_URL}/box_label/${useId}`, {
       method: "post",
       mode: "cors",
@@ -109,26 +109,86 @@ export default function FinsishedProduct() {
       >
         <Container align="center" maxWidth="sm">
           <Box sx={style}>
-          <Grid container align="center" spacing={3}>
-            {no && (
-              <>
-                <Typography align="center"  id="modal-modal-title" variant="h6" component="h2">
-                  THERE IS NO LABEL DATA FOR {productId}
-                </Typography>
-              </>
-            )}
-            {!productDescription && !useId && !no && (
-              <>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                  Enter Product ID
-                </Typography>
-                <Grid container spacing={2}>
+            <Grid container align="center" spacing={3}>
+              {no && (
+                <>
+                  <Typography
+                    align="center"
+                    id="modal-modal-title"
+                    variant="h6"
+                    component="h2"
+                  >
+                    THERE IS NO LABEL DATA FOR {productId}
+                  </Typography>
+                </>
+              )}
+              {!productDescription && !useId && !no && (
+                <>
+                  <Typography
+                    id="modal-modal-title"
+                    variant="h6"
+                    component="h2"
+                  >
+                    Enter Product ID
+                  </Typography>
+                  <Grid container spacing={2}>
+                    <Grid item xs={6} md={8}>
+                      <TextField
+                        label="PRODUCT ID"
+                        type="text"
+                        value={productId}
+                        onChange={(e) => setProductId(e.target.value)}
+                      />
+                    </Grid>
+                    <Grid item xs={6} md={8}>
+                      <Container maxWidth="sm">
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          size="Large"
+                          onClick={() => {
+                            onSubmitId();
+                          }}
+                        >
+                          Save
+                        </Button>
+                      </Container>
+                    </Grid>
+                  </Grid>
+                </>
+              )}
+              {productDescription && !useId && !no && (
+                <>
+                  <Typography
+                    id="modal-modal-title"
+                    variant="h6"
+                    component="h2"
+                  >
+                    {productDescription}
+                  </Typography>
+                  <hr />
                   <Grid item xs={6} md={8}>
                     <TextField
-                      label="PRODUCT ID"
+                      label="LOT"
                       type="text"
-                      value={productId}
-                      onChange={(e) => setProductId(e.target.value)}
+                      value={lot}
+                      onChange={(e) => setLot(e.target.value)}
+                    />
+                  </Grid>
+                  <Grid item xs={6} md={8}>
+                    <TextField
+                      label="BBE"
+                      type="text"
+                      value={bbe}
+                      onChange={(e) => setBbe(e.target.value)}
+                    />
+                  </Grid>
+                  <Grid item xs={6} md={8}>
+                    <TextField
+                      label="BATCH"
+                      type="text"
+                      value={batch}
+                      onChange={(e) => setBatch(e.target.value)}
                     />
                   </Grid>
                   <Grid item xs={6} md={8}>
@@ -138,127 +198,80 @@ export default function FinsishedProduct() {
                         color="primary"
                         size="Large"
                         onClick={() => {
-                          onSubmitId();
+                          onSubmitData();
                         }}
                       >
-                        Save
+                        SAVE LABEL DATA
                       </Button>
                     </Container>
                   </Grid>
-                </Grid>
-              </>
-            )}
-            {productDescription && !useId && !no && (
-              <>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                  {productDescription}
-                </Typography>
+                </>
+              )}
+              {useId && productDescription && !no && (
+                <>
+                  <Typography
+                    align="center"
+                    id="modal-modal-title"
+                    variant="h6"
+                    component="h2"
+                  >
+                    {productDescription}
+                  </Typography>
                   <hr />
-                <Grid item xs={6} md={8}>
-                  <TextField
-                    label="LOT"
-                    type="text"
-                    value={lot}
-                    onChange={(e) => setLot(e.target.value)}
-                  />
-                </Grid>
-                <Grid item xs={6} md={8}>
-                  <TextField
-                    label="BBE"
-                    type="text"
-                    value={bbe}
-                    onChange={(e) => setBbe(e.target.value)}
-                  />
-                </Grid>
-                <Grid item xs={6} md={8}>
-                  <TextField
-                    label="BATCH"
-                    type="text"
-                    value={batch}
-                    onChange={(e) => setBatch(e.target.value)}
-                  />
-                </Grid>
-                <Grid item xs={6} md={8}>
-                  <Container maxWidth="sm">
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      size="Large"
-                      onClick={() => {
-                        onSubmitData();
-                      }}
-                    >
-                      SAVE LABEL DATA
-                    </Button>
-                  </Container>
-                </Grid>
-              </>
-            )}
-            {useId && productDescription && !no && (
-              <>
-                <Typography
-                  align="center"
-                  id="modal-modal-title"
-                  variant="h6"
-                  component="h2"
-                >
-                  {productDescription}
-                </Typography>
+                  <Typography
+                    align="center"
+                    id="modal-modal-title"
+                    variant="p"
+                    component="p"
+                  >
+                    LOT: {lot}
+                  </Typography>
                   <hr />
-                <Typography
-                  align="center"
-                  id="modal-modal-title"
-                  variant="p"
-                  component="p"
-                >
-                  LOT: {lot}
-                </Typography>
+                  <Typography
+                    align="center"
+                    id="modal-modal-title"
+                    variant="p"
+                    component="p"
+                  >
+                    BBE: {bbe}
+                  </Typography>
                   <hr />
-                <Typography
-                  align="center"
-                  id="modal-modal-title"
-                  variant="p"
-                  component="p"
-                >
-                  BBE: {bbe}
-                </Typography>
+                  <Typography
+                    align="center"
+                    id="modal-modal-title"
+                    variant="p"
+                    component="p"
+                  >
+                    BATCH: {batch}
+                  </Typography>
                   <hr />
-                <Typography
-                  align="center"
-                  id="modal-modal-title"
-                  variant="p"
-                  component="p"
-                >
-                  BATCH: {batch}
-                </Typography>
-                  <hr />
-              <br />
-                <Grid align="center" item xs={6} md={8}>
-                  <TextField
-                    label="Quantity"
-                    type="number"
-                    value={qty}
-                    onChange={(e) => setQty(e.target.value)}
-                  />
-                </Grid>
-              <br />
-                <Grid align="center" item xs={6} md={8}>
-                  <Container maxWidth="sm">
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      size="Large"
-                      onClick={() => {
-                        onPrintLabels();
-                      }}
-                    >
-                      PRINT LABELS
-                    </Button>
-                  </Container>
-                </Grid>
-              </>
-            )}
-                </Grid>
+                  <br />
+                  <Grid align="center" item xs={6} md={8}>
+                    <TextField
+                      label="Quantity"
+                      type="number"
+                      value={qty}
+                      onChange={(e) => setQty(e.target.value)}
+                    />
+                  </Grid>
+                  <br />
+                  <Grid align="center" item xs={6} md={8}>
+                    <Container maxWidth="sm">
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        size="Large"
+                        onClick={() => {
+                          onPrintLabels();
+                        }}
+                      >
+                        PRINT LABELS
+                      </Button>
+                    </Container>
+                  </Grid>
+                </>
+              )}
+            </Grid>
           </Box>
         </Container>
       </Modal>
