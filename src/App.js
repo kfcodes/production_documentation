88d1552770f2cefd430f10fd36609a68da39f441
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-12-02 22:14:09 +0000

    Imported new module for creating new finished product

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import DragDropFile from "./components/uploadGS1/UploadPDFLables";
 import UploadDataFiles from "./components/uploadData/UploadDataFiles";
 import LatestPallets from "./components/pallets/palletLists/LatestPallets";
 import SearchPallets from "./components/pallets/searchPalletModal/SearchPallet";
-import FinishedProduct from "./components/finishedProducts/FinishedProduct";
+// import FinishedProduct from "./components/finishedProducts/FinishedProduct";
+import FinishedProduct from "./components/productionSchedule/FinishedProduct";
 import CombinePallets from "./components/pallets/combinePallets/CombinePallets";
 
 
@@ -30,7 +31,7 @@ function App() {
         <Route path="upload_Data/" element={<UploadDataFiles />} />
         <Route path="latest_pallets/" element={<LatestPallets />} />
         <Route path="search_pallet/" element={<SearchPallets />} />
-        <Route path="finished_product/" element={<FinishedProduct />} />
+        <Route path="production/:id" element={<FinishedProduct />} />
         <Route path="combine_pallets/" element={<CombinePallets />} />
       </Routes>
     </>
