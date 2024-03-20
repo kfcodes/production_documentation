ca894c16b29d5a4cc366915f0714d4ce49b44c6d
kfcodes <87446283+kfcodes@users.noreply.github.com>
2024-03-20 14:12:47 +0000

    Removed unused code from pallet components

--- src/components/pallets/combinePallets/CombinePallets.js
+++ src/components/pallets/combinePallets/CombinePallets.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import TextField from "@mui/material/TextField";
