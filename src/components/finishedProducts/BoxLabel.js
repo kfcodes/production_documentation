b1b8cd8e011a3afabbbc4bccb49d1cfd90dc0ac6
kfcodes <87446283+kfcodes@users.noreply.github.com>
2024-03-20 11:34:55 +0000

    Removed unused code from modules

--- src/components/finishedProducts/BoxLabel.js
+++ src/components/finishedProducts/BoxLabel.js
@@ -1,23 +1,8 @@
-// import { useForm } from "react-hook-form";
-import { Outlet, useNavigate, redirect } from "react-router-dom";
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import PrintLabeLButton from "./print_pallet_label_button";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import IconButton from "@mui/material/IconButton";
-import Input from "@mui/material/Input";
-import FilledInput from "@mui/material/FilledInput";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import InputLabel from "@mui/material/InputLabel";
-import InputAdornment from "@mui/material/InputAdornment";
-import FormHelperText from "@mui/material/FormHelperText";
-import FormControl from "@mui/material/FormControl";
 import TextField from "@mui/material/TextField";
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import Button from "@mui/material/Button";
-import { __esModule } from "quagga";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
@@ -34,65 +19,19 @@ const style = {
   p: 4,
 };
 
-// const printPalletLabel = (id) => {
-//   console.log(id);
-//   fetch(`${process.env.REACT_APP_API_URL}/label/${id}`, {
-//     method: "get",
-//     headers: {
-//       Accept: "application/json",
-//       "Content-Type": "application/json",
-//     },
-//   }).then((response) => {
-//     console.log(response);
-//   });
-// };
-//
-// const printBoxLabel = (id) => {
-//   fetch(`${process.env.REACT_APP_API_URL}/box_label/${id}`, {
-//     method: "GET",
-//     headers: {
-//       Accept: "application/json",
-//       "Content-Type": "application/json",
-//     },
-//   }).then((response) => {
-//     console.log(response);
-//   });
-// };
-
 export default function PrintBoxLabel(props) {
   const [eolId] = useState(props.eolId);
   const [batch, setBatch] = useState("");
   const [quantity, setQuantity] = useState(0);
-
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-
-  // Get the EOL details and add them to the state
-  // useEffect(() => {
-  //   fetch(`${process.env.REACT_APP_API_URL}/eol/${eolId}`)
-  //     .then((res) => res.json())
-  //     .then(
-  //       (result) => {
-  //         console.log(result);
-  //         setProductId(result[0].product_id);
-  //         setPo(result[0].po);
-  //         setLot(result[0].lot);
-  //         setBbe(result[0].bbe);
-  //         setQuantity(result[0].quantity);
-  //       },
-  //       (error) => {
-  //         console.log(error);
-  //       }
-  //     );
-  // }, []);
 
   const onSubmit = (eol_id) => {
     let label = {
       batch: batch,
       label_quantity: quantity,
     };
-    // console.log(label);
     fetch(`${process.env.REACT_APP_API_URL}/box_label/${eolId}`, {
       method: "post",
       headers: {
--- src/components/finishedProducts/FinishedProductModal.js
+++ src/components/finishedProducts/FinishedProductModal.js
@@ -1,21 +1,8 @@
-// import { useForm } from "react-hook-form";
-import { Outlet, useNavigate, redirect } from "react-router-dom";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import PrintLabeLButton from "./print_pallet_label_button";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import IconButton from "@mui/material/IconButton";
-import Input from "@mui/material/Input";
-import FilledInput from "@mui/material/FilledInput";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import InputLabel from "@mui/material/InputLabel";
-import InputAdornment from "@mui/material/InputAdornment";
-import FormHelperText from "@mui/material/FormHelperText";
-import FormControl from "@mui/material/FormControl";
 import TextField from "@mui/material/TextField";
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import Button from "@mui/material/Button";
 import { __esModule } from "quagga";
 import Grid from "@mui/material/Grid";
@@ -34,21 +21,7 @@ const style = {
   p: 4,
 };
 
-const printPalletLabel = (id) => {
-  console.log(id);
-  fetch(`${process.env.REACT_APP_API_URL}/label/${id}`, {
-    method: "get",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  }).then((response) => {
-    console.log(response);
-  });
-};
-
 const createEol = (id) => {
-  // This code should hit the api and put the product in the db and create the id passing in the user data
   console.log(id);
 };
 
@@ -65,7 +38,6 @@ const printBoxLabel = (id) => {
 };
 
 export default function Eol(props) {
-  // const { register, handleSubmit, setValue } = useForm();
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -77,86 +49,10 @@ export default function Eol(props) {
   const [productId, setProductId] = useState("");
   const [propsProductId, setPropsProductId] = useState(props.productId);
   const [eolId, setEolId] = useState(props.eolId);
-  // const [productId] = useState("pbn5000");
   const [po, setPo] = useState("");
   const [propsPo, setPropsPo] = useState(props.po);
-  // const [po] = useState("1");
   const palletId = useParams();
   const pId = palletId["palletid"];
-
-  // Get the EOL details and add them to the state
-  // useEffect(() => {
-  //   fetch(`${process.env.REACT_APP_API_URL}/eol/${eolId}`)
-  //     .then((res) => res.json())
-  //     .then(
-  //       (result) => {
-  //         setLot(result[0].lot);
-  //         setBbe(result[0].bbe);
-  //         setQuantity(result[0].quantity);
-  //         setProductId(result[0].productId);
-  //         setEolId(result[0].eolId);
-  //         setPo(result[0].po);
-  //       },
-  //       (error) => {
-  //         console.log(error);
-  //       }
-  //     );
-  // }, []);
-
-  console.log("Props");
-  console.log(props.po);
-  console.log(po);
-  console.log("product ID");
-  console.log(props.productId);
-  console.log(productId);
-  console.log("EOL");
-  console.log(props.eolId);
-  console.log(eolId);
-
-  // Get the details for the pallet
-  // useEffect(() => {
-  //   fetch(`${process.env.REACT_APP_API_URL}/pallet/${pId}`)
-  //     .then((res) => res.json())
-  //     .then(
-  //       (result) => {
-  //         setPallet_id(result[0].pallet_id);
-  //         setPalletType(result[0].pallet_type);
-  //         setEmptyweight(result[0].empty_weight);
-  //         setWeight(result[0].weight);
-  //         setHeight(result[0].height);
-  //         setPacking_list(result[0].packing_list);
-  //       },
-  //       (error) => {
-  //         console.log(error);
-  //       }
-  //     );
-  // }, []);
-
-  // const onSubmit = () => {
-  //   let palletData = {
-  //     pallet_type: palletType,
-  //     empty_weight: emptyweight,
-  //     weight: weight,
-  //     height: height,
-  //     packing_list: packing_list,
-  //   };
-  //   console.log(palletData);
-  //   fetch(`${process.env.REACT_APP_API_URL}/pallet/${pallet_id}`, {
-  //     method: "put",
-  //     mode: "cors",
-  //     headers: {
-  //       Accept: "application/json",
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify(palletData),
-  //   }).then((res) => res.json());
-  // };
-  // <Box sx={{ flexGrow: 1 }}>
-  //
-  //   <Button
-  // onClick={handleOpen}>
-  // End Of line Data
-  // </Button>
 
   if (eolId) {
     return (
