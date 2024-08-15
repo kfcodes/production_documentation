4074b326610adb3bd5e26e9d78f58215642c93fe
kfcodes <87446283+kfcodes@users.noreply.github.com>
2024-08-15 15:16:12 +0100

    Refactored the routes to dedicated module

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+import { Routes, Route } from "react-router-dom";
+import PalletList from "./components/pallets/palletLists/RecentPalletsList";
+import FullPalletList from "./components/pallets/palletLists/AllPalletsList";
+import CreatePalletItem from "./components/pallets/singlePalletDetails/SinglePalletItemsList";
+import CreateNewPalletButton from "./components/pallets/buttons/CreateNewPalletButton";
+import SinglePallet from "./components/pallets/singlePalletDetails/SinglePalletDetails";
+import Production from "./components/productionSchedule/CurrentProduction";
+import FullMps from "./components/productionSchedule/TotalProduction";
+import DragDropFile from "./components/uploadGS1/UploadPDFLables";
+import UploadDataFiles from "./components/uploadData/UploadDataFiles";
+import LatestPallets from "./components/pallets/palletLists/LatestPallets";
+import SearchPallets from "./components/pallets/searchPalletModal/SearchPallet";
+import CombinePallets from "./components/pallets/combinePallets/CombinePallets";
+
+const RoutesComponent = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<PalletList />} />
+        <Route path="/all_pallets" element={<FullPalletList />} />
+        <Route path="create_New_pallet" element={<CreateNewPalletButton />} />
+        <Route path="pallet/:palletid" element={<SinglePallet />}>
+          <Route path="pallet_item" element={<CreatePalletItem />} />
+        </Route>
+        <Route path="production/" element={<Production />} />
+        <Route path="full_mps/" element={<FullMps />} />
+        <Route path="upload_pdf/" element={<DragDropFile />} />
+        <Route path="upload_Data/" element={<UploadDataFiles />} />
+        <Route path="latest_pallets/" element={<LatestPallets />} />
+        <Route path="search_pallet/" element={<SearchPallets />} />
+        <Route path="combine_pallets/" element={<CombinePallets />} />
+      </Routes>
+    </>
+  );
+}
+
+export default RoutesComponent;
