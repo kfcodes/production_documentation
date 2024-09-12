22f87d2f71b842e42e92513825a3c58db7722c48
kfcodes <87446283+kfcodes@users.noreply.github.com>
2024-09-12 12:02:36 +0100

    Added packing list component to router

--- src/routes/routescomponent.js
+++ src/routes/routescomponent.js
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "../components/header/Header"
 import PalletList from "../pages/PalletListPage"
 import SinglePalletPage from "../pages/SinglePalletPage";
-import PackingListsPage from "../pages/PackingListsPage";
+import PackingListHeader from "../pages/individualPackingListData";
 
 const RoutesComponent = () => {
   return (
@@ -16,6 +16,7 @@ const RoutesComponent = () => {
         <Route path="/" element={<PalletList />} />
         <Route path="pallet/:palletid" element={<SinglePalletPage />} />
         <Route path="/packing_lists" element={<PackingListsPage />} />
+        <Route path="/packing_list/:id" element={<PackingListHeader />} />
       </Routes >
     </>
   );
